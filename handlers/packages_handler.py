b22f6460d815fe4cddc903cde8e583e8ece60cdb
mathix <mathixx@gmail.com>
2018-12-24 11:59:57 +0100

    Uploading file to temporary location

--- handlers/packages_handler.py
+++ handlers/packages_handler.py
@@ -1,4 +1,5 @@
 from tornado.web import RequestHandler
+import tempfile
 
 from package_management.package_manager import PackageManager
 
@@ -10,8 +11,12 @@ class PackagesHandler(RequestHandler):
         self.package_manager = package_manager
 
     def post(self):
-        self.request.files[]
+        file = self.request.files['package'][0]
+        original_filename = file['filename']
 
+        with tempfile.NamedTemporaryFile(delete=False) as tmp:
+            tmp.write(file['body'])
+            print(f'file uploaded: {tmp.name}')
 
         self.clear()
         self.set_status(201)
