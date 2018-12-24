ad01f1b84c3f626d2d509cd3dc80855d7f0e796a
mathix <mathixx@gmail.com>
2018-12-24 16:29:59 +0100

    Adding comment on what to do

--- handlers/packages_handler.py
+++ handlers/packages_handler.py
@@ -17,7 +17,6 @@ async def save_file_body_to_temporary_file_async(b: bytes) -> str:
     return await IOLoop.current().run_in_executor(None, save_file_body_to_temporary_file, b)
 
 
-
 class PackagesHandler(RequestHandler):
     package_manager: PackageManager
 
@@ -29,6 +28,11 @@ class PackagesHandler(RequestHandler):
 
         temp_file_path = await save_file_body_to_temporary_file_async(file['body'])
         await self.package_manager.add_package(temp_file_path)
+
+        # 1. receive temp file
+        # 2. read metadata file, parse it and store it in mem
+        # 3. craete appropriate diractories structure for package version
+        # 4. move temp file to new directory and create metadata file
 
         self.clear()
         self.set_status(201)
